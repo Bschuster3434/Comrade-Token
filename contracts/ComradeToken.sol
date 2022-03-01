41a8baf05a0fc0a92bfbb27467f491cac0f9dedc
Brian Schuster <brian.daniel.schuster@gmail.com>
2022-03-01 12:12:51 -0500

    feat: Adding tax to transfer

--- contracts/ComradeToken.sol
+++ contracts/ComradeToken.sol
@@ -1,11 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract ComradeToken is ERC20, ERC20Burnable {
-    constructor() ERC20("Comrade Token", "COMRADE") {
+    uint16 protocolPerc;
+    address payable private protocolWallet;
+
+    constructor(uint16 _protocolPerc, address payable _protocolWallet) ERC20("Comrade Token", "COMRADE") {
         _mint(msg.sender, 1000000000 * 18 ** decimals());
+        protocolPerc = _protocolPerc; // protocol percentage is ( protocalPerc / 10 ** 4)
+        protocolWallet = _protocolWallet;
     }
+
+    // Overrides the transfer function for the ERC20 tokens
+    // In order for the transfer to be allowed, a protocol fee
+    // Needs to be paid upon send
+    function transfer(address _to, uint256 _amount) public override returns (bool) {
+        address owner = _msgSender();
+        require(balanceOf(owner) >= _amount, "ERC20: transfer amount exceeds balance");
+        
+        uint256 protocolFee = (_amount * protocolPerc) / (10 ** 4);
+        uint256 amountPlusProtocolFee = _amount + protocolFee;
+        require(balanceOf(owner) >= amountPlusProtocolFee, "COMRADE: Cannot pay protocol fee"); // balance of tokens must be greater than the amount + protocol fee
+
+        
+        _transfer(owner, protocolWallet, protocolFee);
+        _transfer(owner, _to, _amount);
+        return true;
+    }    
 }
--- test/Token.js
+++ test/Token.js
@@ -3,16 +3,20 @@ const { expect } = require("chai");
 describe("Token contract", function () {
     let Token;
     let comradeToken;
+    let protocolPerc;
     let owner;
     let addr1;
     let addr2;
+    let protocolWallet;
     let addrs;
 
   beforeEach(async function () {
-    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+    [owner, addr1, addr2, protocolWallet, ...addrs] = await ethers.getSigners();
+
+    protocolPerc = 1000; //10% Fee
 
     Token = await ethers.getContractFactory("ComradeToken");
-    comradeToken = await Token.deploy();
+    comradeToken = await Token.deploy(protocolPerc, protocolWallet.address);
 
   });
   describe("Deployment", function () {
@@ -30,15 +34,15 @@ describe("Token contract", function () {
   describe("Transactions", function () {
     it("Should transfer tokens between accounts", async function () {
       // Transfer 50 tokens from owner to addr1
-      await comradeToken.transfer(addr1.address, 50);
+      await comradeToken.transfer(addr1.address, ethers.utils.parseEther("50"));
       const addr1Balance = await comradeToken.balanceOf(addr1.address);
-      expect(addr1Balance).to.equal(50);
+      expect(addr1Balance).to.equal(ethers.utils.parseEther("50"));
 
       // Transfer 50 tokens from addr1 to addr2
       // We use .connect(signer) to send a transaction from another account
-      await comradeToken.connect(addr1).transfer(addr2.address, 50);
+      await comradeToken.connect(addr1).transfer(addr2.address, ethers.utils.parseEther("40"));
       const addr2Balance = await comradeToken.balanceOf(addr2.address);
-      expect(addr2Balance).to.equal(50);
+      expect(addr2Balance).to.equal(ethers.utils.parseEther("40"));
     });
 
     it("Should fail if sender doesn’t have enough tokens", async function () {
@@ -48,7 +52,7 @@ describe("Token contract", function () {
       // `require` will evaluate false and revert the transaction.
       await expect(
         comradeToken.connect(addr1).transfer(owner.address, 1)
-      ).to.be.revertedWith("Not enough tokens");
+      ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
 
       // Owner balance shouldn't have changed.
       expect(await comradeToken.balanceOf(owner.address)).to.equal(
@@ -59,21 +63,33 @@ describe("Token contract", function () {
     it("Should update balances after transfers", async function () {
       const initialOwnerBalance = await comradeToken.balanceOf(owner.address);
 
-      // Transfer 100 tokens from owner to addr1.
-      await comradeToken.transfer(addr1.address, 100);
+      // Transfer 100000 tokens from owner to addr1.
+      await comradeToken.transfer(addr1.address, 100000);
 
       // Transfer another 50 tokens from owner to addr2.
-      await comradeToken.transfer(addr2.address, 50);
+      await comradeToken.transfer(addr2.address, 50000);
 
       // Check balances.
       const finalOwnerBalance = await comradeToken.balanceOf(owner.address);
-      expect(finalOwnerBalance).to.equal(initialOwnerBalance.sub(150));
+      expect(finalOwnerBalance).to.equal(initialOwnerBalance.sub(150000 + 15000));
 
       const addr1Balance = await comradeToken.balanceOf(addr1.address);
-      expect(addr1Balance).to.equal(100);
+      expect(addr1Balance).to.equal(100000);
 
       const addr2Balance = await comradeToken.balanceOf(addr2.address);
-      expect(addr2Balance).to.equal(50);
+      expect(addr2Balance).to.equal(50000);
     });
 
-});
+    it("Should tax the sender when sending out a fee", async function () {
+      await comradeToken.transfer(addr1.address, ethers.utils.parseEther("100"));
+
+      expect(await comradeToken.balanceOf(addr1.address)).to.equal(ethers.utils.parseEther("100"));
+      
+      await comradeToken.connect(addr1).transfer(addr2.address, ethers.utils.parseEther("10"));
+
+      expect(await comradeToken.balanceOf(addr2.address)).to.equal(ethers.utils.parseEther("10"));
+      expect(await comradeToken.balanceOf(addr1.address)).to.equal(ethers.utils.parseEther("89")); // 10 COMRADE - 1 COMRADE Fee
+      expect(await comradeToken.balanceOf(protocolWallet.address)).to.equal(ethers.utils.parseEther("11"));
+    })
+  });
+})
